0a3557c9493ec54143d94dd67234421ad6d7ee25
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2022-12-20 16:49:09 -0500

    Add error handling

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ let targetAbvInputEl = document.querySelector("#target-abv");
 let currVolInputEl = document.querySelector("#curr-vol")
 let form = document.querySelector("form");
 let resultContainerEl = document.querySelector(".result-container");
+let errorEl = document.querySelector(".error");
 
 const renderResult = (oz, ml) => {
+    errorEl.style.display = "none";
     resultContainerEl.style.display = "block";
 
     document.querySelector(".result-oz").innerHTML = oz + "oz";
     document.querySelector(".result-ml").innerHTML = ml + "ml";
+}
+
+const handleError = () => {
+    resultContainerEl.style.display = "none";
+    errorEl.style.display = "block";
 }
 
 const handleFormSubmit = e => {
@@ -17,6 +24,11 @@ const handleFormSubmit = e => {
     const currAbv = parseFloat(currAbvInputEl.value)
     const targetAbv = parseFloat(targetAbvInputEl.value)
     const currVol = parseFloat(currVolInputEl.value)
+
+    if (currAbv < targetAbv) {
+        handleError();
+        return;
+    }
 
     let resultOz = ((currVol * currAbv) / targetAbv) - currVol;
     resultOz = Math.round(resultOz * 100) / 100;
